750accf00d118bfbc4f32fb7985237b1a678c202
nandikantipavan <89066315+nandikantipavan@users.noreply.github.com>
2021-10-12 20:36:11 +0530

    Update AddPage.java

--- src/main/java/org/cysecurity/cspf/jvl/controller/AddPage.java
+++ src/main/java/org/cysecurity/cspf/jvl/controller/AddPage.java
@@ -3,7 +3,7 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor. edit test        
  */ 
-
+  
 package org.cysecurity.cspf.jvl.controller;
 
 import java.io.BufferedWriter;
